b1195886daf110a706bf5702db5855dfcc483ff8
osmonduu <osmondwa@gmail.com>
2023-08-21 15:18:47 -0400

    finished html

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 import "./style.css"
+import { getWeather } from "./weather"
+
+getWeather(10,10, Int1.DateTimeFomat().resolvedOptions().timeZone).then(res => {
+    console.log(res.data)
+})
---
+++ weather.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+//https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime
+
+
+export function getWeather(lat, lon, timezeone) {
+    axios.get(
+        "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
+        {
+            params: {
+                lattitude: lat,
+                longitutde: lon,
+                timezone,
+    }})
+}
