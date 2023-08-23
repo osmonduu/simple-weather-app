8d49607a1d4194fd19f06995f495a320c85336a5
osmonduu <osmondwa@gmail.com>
2023-08-23 15:10:13 -0400

    finished parsing current, daily, and hourly data

--- main.js
+++ main.js
@@ -15,6 +15,23 @@ function renderWeather({ current, daily, hourly }) {
     document.body.classList.remove("blurred")
 }
 
+function setValue(selector, value, { parent = document } = {}) {
+    parent.querySelector(`[data-${selector}]`).textContent = value
+}
+
+function getIconUrl(iconCode) {
+
+}
+
+const currentIcon = document.querySelector("[data-current-icon]")
 function renderCurrentWeather(current) {
-    
+    currentIcon.src = getIconUrl(current.iconCode)
+    //document.querySelector("[data-current-temp]").textContent = current.currentTemp
+    setValue("current-temp", current.currentTemp)
+    setValue("current-high", current.highTemp)
+    setValue("current-low", current.lowTemp)
+    setValue("current-fl-high", current.highFeelsLike)
+    setValue("current-fl-low", current.lowFeelsLike)
+    setValue("current-wind", current.windSpeed)
+    setValue("current-precip", current.precip)
 }
